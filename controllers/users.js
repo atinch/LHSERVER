7cf3fce00c78f1cade423cb9ca0b9012d18b806b
atinc <atincerguven@gmail.com>
2019-04-06 18:11:09 +0100

    first commit

---
+++ controllers/users.js
@@ -0,0 +1,18 @@
+module.exports = {
+  signUp: async (req, res, next) => {
+    console.log('UsersController.SignUp() called..');
+/*     try {
+      [OUR CODE]
+    } catch (error) {
+      next(error);
+    } */
+  },
+
+signIn: async (req, res, next) => {
+  console.log('UsersController.SignIn() called..');
+  },
+  
+secret: async (req, res, next) => {
+    console.log('UsersController.secret() called..');
+  }
+}
